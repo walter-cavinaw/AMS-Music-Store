33ad001046103d82230c1c0c8fa35fc7b607412f
Yago Arroyo <yagoag@gmail.com>
2014-11-23 02:30:34 -0800

    Added paging to list_items

--- src/list_items.php
+++ src/list_items.php
@@ -9,14 +9,34 @@
     $query->execute();
     $result = $query->get_result();
 
+    if (isset($_GET['page']))
+        $page = $_GET['page'] - 1;
+    else
+        $page = 0;
+
+    $i = 0;
     while ($row = $result->fetch_assoc()) {
-        if (!file_exists('img/covers/'.$row['upc'].'.jpg'))
-            $cover = "unknown";
-        else
-            $cover = $row['upc'];
-        
-        echo '<a href="?p=display_item&id='.$row['upc'].'" class="item"><div class="cover"><img src="img/covers/'.$cover.'.jpg" width="64px" height="64px" /></div><div class="info"><h2>'.$row['title'].'</h2>'.$row['itemtype'].'<br />'.$row['category'].'<br />'.$row['releaseyear'].'</div><div class="price"><h2>$'.number_format($row['price'], 2).'</h2></div></a>';
+        if ($i >= $page * 6 && $i <= $page * 6 + 5 ) {
+            if (!file_exists('img/covers/'.$row['upc'].'.jpg'))
+                $cover = "unknown";
+            else
+                $cover = $row['upc'];
+
+            echo '<a href="?p=display_item&id='.$row['upc'].'" class="item">';
+            echo '<div class="cover"><img src="img/covers/'.$cover.'.jpg" width="64px" height="64px" /></div>';
+            echo '<div class="info"><h2>'.$row['title'].'</h2>'.$row['itemtype'].'<br />'.$row['category'].'<br />'.$row['releaseyear'].'</div>';
+            echo '<div class="price"><h2>$'.number_format($row['price'], 2).'</h2></div></a>';
+        }
+
+        $i++;
     }
 
-    // TODO: Add paging to the table. Possible idea: use JavaScript not to have to reload pages and complete a new query every page.
+    echo '<div id="pager">Page: ';
+    for ($i = 1; $i <= ceil($result->num_rows / 6); $i++) {
+        echo ' <a href="?';
+        if (isset($_GET['cat']))
+            echo 'cat='.$_GET['cat'].'&';
+        echo 'page='.$i.'">[ '.$i.' ]</a> ';
+    }
+echo '</div>';
 ?>
