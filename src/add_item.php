f2e963b181f83fd44450329c773d51fb90c11ba9
Jonathan <mcgrandlej@gmail.com>
2014-11-24 15:59:47 -0800

    Create add item page

---
+++ src/add_item.php
@@ -0,0 +1,7 @@
+<form id="add_item" action="?act=add_item" method="post">
+    <h1>Add Item</h1>
+    <p><input type="text" name="upc" placeholder="UPC" size="40" /></p>
+    <p><input type="text" name="quantity" placeholder="Quantity" size="40" /></p>
+    <p><input type="text" name="price" placeholder="Price (Optional)" size="20" /></p>
+    <p><input type="submit" value="Add!" /></p>
+</form>
