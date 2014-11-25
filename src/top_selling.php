905c94e5cec9fdc9540c7e8fb6b4526bfc32fc9a
Jonathan <mcgrandlej@gmail.com>
2014-11-25 14:32:40 -0800

    Start of top selling items

---
+++ src/top_selling.php
@@ -0,0 +1,6 @@
+<form id="top_selling" action="?act=top_selling" method="post">
+    <h1>Get top selling items</h1>
+    <p><input type="text" name="date" placeholder="Date of Interest" size="40" /></p>
+    <p><input type="text" name="number" placeholder="Number of bestsellers to display" size="40" /></p>
+    <p><input type="submit" value="View" /></p>
+</form>
