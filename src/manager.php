bd4811da791c5c4de1cd19d14596e1b0816865b4
Jonathan <mcgrandlej@gmail.com>
2014-11-24 16:00:14 -0800

    Create manager page

---
+++ src/manager.php
@@ -0,0 +1,9 @@
+<?php
+    
+    echo '<h1>Welcome Manager!</h1>';
+    echo '    <p>What would you like to do:</p>';
+    echo '    <p><a href="?p=add_item">Add an item</a></p>';
+    echo '    <p><a href="">Process a delivery</a></p>';
+    echo '    <p><a href="">View a daily sales report</a></p>';
+    echo '    <p>The current top selling items are:</p>';
+?>
