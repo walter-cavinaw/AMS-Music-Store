2afa4872126c670a4908aaa7768db24b0ad3a2e7
Jonathan <mcgrandlej@gmail.com>
2014-11-24 16:11:52 -0800

    Add process delivery

---
+++ src/process_delivery.php
@@ -0,0 +1,7 @@
+<form id="process_delivery" action="?act=process_delivery" method="post">
+    <h1>Process Delivery</h1>
+    <p><input type="text" name="rid" placeholder="Receipt ID" size="40" /></p>
+    <p><input type="text" name="date" placeholder="Date Delivered" size="40" /></p>
+    <p><input type="submit" value="Process" /></p>
+    <p>Delivery date must match YYMMDD</p>
+</form>
