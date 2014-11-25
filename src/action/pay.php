419df81ed3a37148dbf34ed55d0a040493549f3b
Jonathan <mcgrandlej@gmail.com>
2014-11-25 11:38:25 -0800

    Update delivery time to reflect "This number is estimated by the number of outstanding orders and  the maximum number of orders that can be delivered in a day (which is fixed; you need to decide on this)."

--- src/action/pay.php
+++ src/action/pay.php
@@ -18,8 +18,18 @@
                 echo '<p>One of more of your items are not in stock at the moment.</p>';
                 echo '<p>Please, try buying a lower ammount or come back later!</p>';
             } else {
-                $query = $connection->prepare('INSERT INTO purchase (pdate, cid, cardNumber, expiryDate, expectedDate) VALUES (CURDATE(), ?, ?, ?, DATE_ADD(CURDATE(), INTERVAL 2 DAY))');
-                $query->bind_param('sss', $_SESSION['username'], $_POST['card_number'], $_POST['expiry_date']);
+                //Get the number of pending orders
+                $query = $connection->prepare('SELECT * FROM purchase WHERE deliveredDate IS NULL AND expectedDate IS NOT NULL'); //Find outstanding orders
+                $query->execute();
+                $numOutstandingOrders = $query->get_result()->num_rows;
+                var_dump($numOutstandingOrders);
+                $expectedDelay= strval(2 + round($numOutstandingOrders/6)); //Say standard is two days and we can deliver 6 per day
+                $query->close(); //Need to close it so we can execute the next query
+                $query = $connection->prepare('INSERT INTO purchase (pdate, cid, cardNumber, expiryDate, expectedDate) VALUES (CURDATE(), ?, ?, ?, DATE_ADD(CURDATE(), INTERVAL ?  DAY))');
+                if(!$query){
+                    echo ' : ' . $connection->error;
+                }
+                $query->bind_param('ssss', $_SESSION['username'], $_POST['card_number'], $_POST['expiry_date'],$expectedDelay);
                 $query->execute();
 
                 if ($query->affected_rows >= 1) {
@@ -38,7 +48,7 @@
                     }
                     
                     echo '<h1>Checkout successful</h1><br />';
-                    echo '<p>Your payment has been processed successfully.</p><p>You can now wait for the items in your address in two days.</p><br />';
+                    echo '<p>Your payment has been processed successfully.</p><p>You can now wait for the items at your address in ' . $expectedDelay .' days.</p><br />';
                     echo '<p>Your transaction number is <strong>'.$receipt_id.'</strong>.</p><p>Write it down in case you need to contact us about your order.</p><br />';
                     echo '<p>Have a great day and consider taking a look at our <a href="index.php">other offers</a>.</p>';
                     
