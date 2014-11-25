3fccdd4eabd34806ae8a94fdc1de9e85aee3f672
Jonathan <mcgrandlej@gmail.com>
2014-11-24 16:46:58 -0800

    Add daily report pages

---
+++ src/daily_report.php
@@ -0,0 +1,6 @@
+<form id="daily_report" action="?act=daily_report" method="post">
+    <h1>Get daily report</h1>
+    <p><input type="text" name="date" placeholder="Date of Interest" size="40" /></p>
+    <p><input type="submit" value="Process" /></p>
+    <p>Delivery date must match YYMMDD</p>
+</form>
--- src/manager.php
+++ src/manager.php
@@ -4,6 +4,6 @@
     echo '    <p>What would you like to do:</p>';
     echo '    <p><a href="?p=add_item">Add an item</a></p>';
     echo '    <p><a href="?p=process_delivery">Process a delivery</a></p>';
-    echo '    <p><a href="">View a daily sales report</a></p>';
+    echo '    <p><a href="?p=daily_report">View a daily sales report</a></p>';
     echo '    <p>The current top selling items are:</p>';
 ?>
