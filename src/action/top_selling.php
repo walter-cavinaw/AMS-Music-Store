62bbf10ac28ca879bd0672140be77ad87452cbc3
Jonathan <mcgrandlej@gmail.com>
2014-11-25 17:03:24 -0800

    Fix ordering and only displaying n items

--- src/action/top_selling.php
+++ src/action/top_selling.php
@@ -12,12 +12,13 @@
         echo '<h1>Total sales for ' . $_POST['date'] . '</h1>';
         echo '<table width=100%>';
         echo '   <tr>';
-        echo '      <th width=25%>Title</th>';
-        echo '      <th width=25%>Company</th>';
-        echo '      <th width=25%>Current Stock</th>';
-        echo '      <th width=25%>Units Sold That Day</th>';
+        echo '      <th width=25% align="center">Title</th>';
+        echo '      <th width=25% align="center">Company</th>';
+        echo '      <th width=25% align="center">Current Stock</th>';
+        echo '      <th width=25% align="center">Units Sold That Day</th>';
         echo '   </tr>';
         
+        $upcQuantityArray;
         //Step through each upc, getting the details for it, and summing the number of sold that day
         while ($row = $upcs->fetch_assoc()) {
             //Get the number sold that day
@@ -27,26 +28,36 @@
             $quantity = $query->get_result()->fetch_row();
             $query->close();
             
+            //Store the upc and quanity in the array
+            $upcQuantityArray[$row['upc']] = $quantity[0];
+        }
+        
+        //Sort the array according to value, highest value first
+        arsort($upcQuantityArray);
+        $keys = array_keys($upcQuantityArray); //Need to access the array with an index. Each element of keys is the key value of upcQuantityArray
+        
+        $i = 0;
+        while($i<$_POST['number']){ //GO through the first n elements as supplied by the user
+            //Get the upc
+            $upc = $keys[$i];
+            
             //Get the upc info
             $query = $connection->prepare('SELECT title, company, stock FROM item where upc = ?');
-            $query->bind_param('s', $row['upc']);
+            $query->bind_param('s', $upc);
             $query->execute();
             $upcInfo = $query->get_result()->fetch_row();
             $query->close();
             
             //Print the info
             echo '   <tr>';
-            echo '      <td>' . $upcInfo[0] .'</td>';
-            echo '      <td>' . $upcInfo[1] .'</td>';
-            echo '      <td>' . $upcInfo[2] .'</td>';
-            echo '      <td>' . $quantity[0] .'</td>';
+            echo '      <td align="right">' . $upcInfo[0] .'</td>';
+            echo '      <td align="right">' . $upcInfo[1] .'</td>';
+            echo '      <td align="right">' . $upcInfo[2] .'</td>';
+            echo '      <td align="right">' . $upcQuantityArray[$upc] .'</td>';
             echo '   </tr>';
+            $i++;
         }
-        
         echo '</table>';
-        
-        
-        
     } else {
         echo '<h1>Error getting top sales</h1>';
         echo '<p>You must specify a date and number of top sellers.</p>';
