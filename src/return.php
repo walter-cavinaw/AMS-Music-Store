cca487acdf6218fc2afa32a8fab5144579c47477
Jonathan <mcgrandlej@gmail.com>
2014-11-25 17:38:31 -0800

    Add return page

---
+++ src/return.php
@@ -0,0 +1,7 @@
+<form id="add_item" action="?act=add_item" method="post">
+    <h1>Process A Return</h1>
+    <p><input type="text" name="rid" placeholder="Receipt ID" size="40" /></p>
+    <p><input type="text" name="upc" placeholder="UPC of Returned Item" size="40" /></p>
+    <p><input type="text" name="quantity" placeholder="Quantity of Returned Item" size="40" /></p>
+    <p><input type="submit" value="Return" /></p>
+</form>
