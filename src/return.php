572cbf33f20a63ad85d0cc5c59809d6103a35376
Jonathan <mcgrandlej@gmail.com>
2014-11-25 17:40:37 -0800

    Fix styling

--- src/return.php
+++ src/return.php
@@ -1,4 +1,4 @@
-<form id="add_item" action="?act=add_item" method="post">
+<form id="process_return" action="?act=process_return" method="post">
     <h1>Process A Return</h1>
     <p><input type="text" name="rid" placeholder="Receipt ID" size="40" /></p>
     <p><input type="text" name="upc" placeholder="UPC of Returned Item" size="40" /></p>
