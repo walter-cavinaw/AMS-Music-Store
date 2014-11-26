f69a04a4a700e66cf8c61b29272d745f9edb2f55
Yago Arroyo <yagoag@gmail.com>
2014-11-25 18:59:48 -0800

    Fixed formatting of Manager's main page

--- src/manager.php
+++ src/manager.php
@@ -1,9 +1,8 @@
-<?php
-    
-    echo '<h1>Welcome Manager!</h1>';
-    echo '    <p>What would you like to do:</p>';
-    echo '    <p><a href="?p=add_item">Add an item</a></p>';
-    echo '    <p><a href="?p=process_delivery">Process a delivery</a></p>';
-    echo '    <p><a href="?p=daily_report">View a daily sales report</a></p>';
-    echo '    <p><a href="?p=top_selling">View top selling items for a given day</a></p>';
-?>
+<div>
+    <h1>Welcome Manager!</h1><br />
+    <p>What would you like to do:</p>
+    <p><a href="?p=add_item">Add an item</a></p>
+    <p><a href="?p=process_delivery">Process a delivery</a></p>
+    <p><a href="?p=daily_report">View a daily sales report</a></p>
+    <p><a href="?p=top_selling">View top selling items for a given day</a></p>
+</div>
