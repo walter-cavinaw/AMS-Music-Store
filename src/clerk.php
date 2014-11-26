d62b6b062ca2d454458cc4f47a51d90365122e0b
walter-cavinaw <w.cavinaw@gmail.com>
2014-11-25 20:14:47 -0800

    formatting for clerk

--- src/clerk.php
+++ src/clerk.php
@@ -1,5 +1,4 @@
-<?php
-    
-    echo '<h1>Welcome Clerk!</h1>';
-    echo '    <p><a href="?p=return">Process a return</a></p>';
-?>
+<div>
+    <h1>Welcome Clerk!</h1><br />
+    <p><a href="?p=return">Process a return</a></p>
+</div>
