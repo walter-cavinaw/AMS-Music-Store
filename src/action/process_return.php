fae31de32d10937e0cd871d0c845ac630b9fcc0a
Jonathan <mcgrandlej@gmail.com>
2014-11-25 18:15:08 -0800

    NOT WORKING. Almost done, I shouldn't be committing but in case anyone wants to work on this.

--- src/action/process_return.php
+++ src/action/process_return.php
@@ -1,33 +1,65 @@
 <div>
 <?php
     if (!empty($_POST['rid']) && !empty($_POST['upc']) && !empty($_POST['quantity'])){
-        //Get the old stock
-        $query = $connection->prepare('SELECT stock FROM item WHERE upc = ?');
-        $query->bind_param('s', $_POST['upc']);
+        //First make sure the receipt ID is within the last 15 days
+        $query = $connection->prepare('SELECT receiptid FROM purchase where ABS(DATEDIFF(pdate, CURDATE())) <= 15 && receiptid = ?');
+        $query->bind_param('s', $_POST['rid']);
         $query->execute();
         $result = $query->get_result();
-        $resultArray = $result->fetch_assoc();
-        $oldStock = $resultArray["stock"];
+        $query->close();
         
-        if($result->num_rows<1){
-            echo '<h1>Error adding item</h1>';
-            echo '<p>No mathcing UPC found.</p>';
+        if($result->num_rows!=1){
+            echo '<h1>Error returning item</h1>';
+            echo '<p>No matching receipt ID found.</p>';
+            echo '<p>Make sure this purchase was within the last 15 days, and you entered the receipt ID correctly.</p>';
             echo '<p><a href="javascript:history.go(-1)">Go Back</a></p>';
             die();
         }
         
-        // Update stocks
-        $query = $connection->prepare('UPDATE item SET stock=? WHERE upc=?;');
-        $query->bind_param('ss', strval($_POST['quantity']+$oldStock), $_POST['upc']);
+        //Next make sure that the UPC is included in the receipt
+        $query = $connection->prepare('SELECT quantity from purchase_item where receiptId = ? and upc = ?');
+        $query->bind_param('ss', $_POST['rid'], $_POST['upc']);
         $query->execute();
+        $result = $query->get_result();
+        $query->close();
         
-        if(!empty($_POST['price'])){
-            $query = $connection->prepare('UPDATE item SET price=? WHERE upc=?;');
-            $query->bind_param('ss', $_POST['price'], $_POST['upc']);
-            $query->execute();
+        if($result->num_rows!=1){
+            echo '<h1>Error returning item</h1>';
+            echo '<p>No matching UPC found in the given receipt ID.</p>';
+            echo '<p>Make sure this purchase included the returned item.</p>';
+            echo '<p><a href="javascript:history.go(-1)">Go Back</a></p>';
+            die();
         }
-        echo '<h1>Succesfully updated items.</h1>';
+        
+        $quantityBought = $result->fetch_row()[0];
+        
+        //Make sure the quantity being returned is less than or equal to the quantity purchased
+        if($quantityBought>=$_POST['quantity']){
+            echo '<h1>Error returning item</h1>';
+            echo '<p>Too many items being returned.</p>';
+            echo '<p>Make sure the quantity being returned is less than or equal to the quantity purchased.</p>';
+            echo '<p><a href="javascript:history.go(-1)">Go Back</a></p>';
+            die();
+        }
+        
+        //Get the highest return ID so far
+        $query = $connection->prepare('SELECT MAX(retid) FROM return_item;');
+        $query->execute();
+        $max = $query->get_result()->fetch_row()[0];
+        $query->close();
+        
+        $retid = $max + 1;
+        //All checks out, make return id
+        $query = $connection->prepare('INSERT INTO return_item VALUES (?, ?, ?)');
+        $query->bind_param('sss', $retid, $_POST['upd'], $_POST['quantity']);
+        $query->execute();
+        $result = $query->get_result();
+        $query->close();
+        
+        echo '<h1>Succesfully returned item(s).</h1>';
+        echo '<p>Return ID is: ' . $retid . '</p>';
         echo '<p><a href="javascript:history.go(-1)">Go Back</a></p>';
+        
     } else {
         echo '<h1>Error returning item</h1>';
         echo '<p>You must specify a Receipt ID, UPC and quantity.</p>';
